91608dc20f4b1cc0b7b20e4acef2a8312be1d6f4
sridhar2977 <sridhar2977@gmail.com>
2018-03-18 15:59:19 +0530

    welcome controller

--- src/main/java/com/aws/codestar/projecttemplates/controller/WelcomeController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/WelcomeController.java
@@ -1,14 +1,18 @@
 package com.aws.codestar.projecttemplates.controller;
 
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/welcome")
 public class WelcomeController {
 
-    @RequestMapping("/welcome")
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return new JSONObject().put("Output", "Greetings from Spring Boot!").toString();
     }
 
 }
